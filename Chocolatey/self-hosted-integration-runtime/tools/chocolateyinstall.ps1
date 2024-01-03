e6081d09559c1e841df458a34452188282aa6bc0
HealisticEngineer <johnedwardhall@gmail.com>
2024-01-03 11:58:22 +0100

    reduce over complication

--- Chocolatey/self-hosted-integration-runtime/tools/chocolateyinstall.ps1
+++ Chocolatey/self-hosted-integration-runtime/tools/chocolateyinstall.ps1
@@ -10,38 +10,16 @@ if ([Version] (Get-CimInstance Win32_OperatingSystem).Version -lt [version] "10.
   Write-Error "Microsoft Integration Runtime requires a minimum of Windows 10 or Windows Server 2016"
 }
 
-# get the filename from the URL
-$filename = [IO.Path]::GetFileName($url)
-
-# Download like Install-ChocolateyPackage (so we can restart from cached download)
-$chocTempDir = $env:TEMP
-
-$tempDir = Join-Path $chocTempDir "$($env:chocolateyPackageName)"
-if ($env:chocolateyPackageVersion -ne $null) { $tempDir = Join-Path $tempDir "$($env:chocolateyPackageVersion)"; }
-$tempDir = $tempDir -replace '\\chocolatey\\chocolatey\\', '\chocolatey\'
-if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-$downloadFilePath = Join-Path $tempDir $filename
-
-$fullFilePath = Join-Path $toolsDir $filename
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
   url           = $url
-  FileFullPath  = $downloadFilePath
-  checksum      = $checksum
-  checksumType  = 'sha256'
-}
-
-# Download the file
-$filePath = Get-ChocolateyWebFile @packageArgs
-
-# Install the package
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  file  = $fullFilePath
-  softwareName  = $softwareName
   silentArgs    = "/quiet /passive"
   validExitCodes= @(0, 3010, 1641)
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  softwareName  = $softwareName
 }
 
 Install-ChocolateyPackage @packageArgs
